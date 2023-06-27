c9b42f9f6b8bec4d19ce7f4f6fdf5aff55b71501
Masayuki Ushio <ong8181@gmail.com>
2023-06-27 13:50:22 +0800

    updated

--- R/data_loader.R
+++ R/data_loader.R
@@ -5,32 +5,47 @@
 #' @examples
 #' # data_loader()
 data_loader <- function(dataset_name) {
-  # Batch loading Ushio (2022) Proceedings B
-  if (dataset_name == "ushio_2022_prsb") {
-    data("ushio_2022_prsb_df_asvtab_copy")
-    data("ushio_2022_prsb_df_climate")
-    data("ushio_2022_prsb_df_sample")
-    data("ushio_2022_prsb_df_taxa")
+  dataset_list <- c("ushio_2022_prsb", "ushio_2023a_elife", "ushio_2023b_elife")
+    # Check arguments
+  if (dataset_name %in% dataset_list) {
+    stop("Invalid dataset name!")
+  }
+
+    # Batch loading Ushio (2022) Proceedings B
+  if (dataset_name == dataset_list[1]) {
+    utils::data("ushio_2022_prsb_df_asvtab_copy")
+    utils::data("ushio_2022_prsb_df_climate")
+    utils::data("ushio_2022_prsb_df_sample")
+    utils::data("ushio_2022_prsb_df_taxa")
+    # Compile date
+    ushio_2022_prsb_df_sample$plot <- factor(ushio_2022_prsb_df_sample$plot)
+    ushio_2022_prsb_df_sample$date <- lubridate::ymd(ushio_2022_prsb_df_sample$date)
   }
 
   # Batch loading Ushio et al. (2023a) eLife
-  if (dataset_name == "ushio_2023a_elife") {
-    data("ushio_2023a_elife_df_asvtab_copy")
-    data("ushio_2023a_elife_df_asvtab_read")
-    data("ushio_2023a_elife_df_sample")
-    data("ushio_2023a_elife_df_taxa")
+  if (dataset_name == dataset_list[2]) {
+    utils::data("ushio_2023a_elife_df_asvtab_copy")
+    utils::data("ushio_2023a_elife_df_asvtab_read")
+    utils::data("ushio_2023a_elife_df_sample")
+    utils::data("ushio_2023a_elife_df_taxa")
+    # Compile date
+    ushio_2023a_elife_df_sample$site_code <- factor(ushio_2023a_elife_df_sample$site_code)
+    ushio_2023a_elife_df_sample$date <- lubridate::ymd(ushio_2023a_elife_df_sample$date)
   }
 
   # Batch loading Ushio et al. (2023b) eLife
-  if (dataset_name == "ushio_2023b_elife") {
-    data("ushio_2023b_elife_df_asvtab_copy")
-    data("ushio_2023b_elife_df_sample")
-    data("ushio_2023b_elife_df_taxa")
-    data("ushio_2023b_elife_df_rice2017")
-    data("ushio_2023b_elife_df_climate2019")
-    data("ushio_2023b_elife_df_rice2019_growth")
-    data("ushio_2023b_elife_df_rice2019_head")
-    data("ushio_2023b_elife_df_rice2019_yield")
+  if (dataset_name == dataset_list[3]) {
+    utils::data("ushio_2023b_elife_df_asvtab_copy")
+    utils::data("ushio_2023b_elife_df_sample")
+    utils::data("ushio_2023b_elife_df_taxa")
+    utils::data("ushio_2023b_elife_df_rice2017")
+    utils::data("ushio_2023b_elife_df_climate2019")
+    utils::data("ushio_2023b_elife_df_rice2019_growth")
+    utils::data("ushio_2023b_elife_df_rice2019_head")
+    utils::data("ushio_2023b_elife_df_rice2019_yield")
+    # Compile date
+    ushio_2023b_elife_df_sample$plot <- factor(ushio_2023b_elife_df_sample$plot)
+    ushio_2023b_elife_df_sample$date <- lubridate::ymd(ushio_2023b_elife_df_sample$date)
   }
 }
 
