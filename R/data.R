f10121efdceb934f4e7d481bd1cfa50ac2c943b5
Masayuki Ushio <ong8181@gmail.com>
2023-06-27 12:23:36 +0800

    Data updated

--- R/data.R
+++ R/data.R
@@ -1,8 +1,11 @@
-#' Data from Ushio (2022) Proc. Royal Soc. B: ASV table
+# ------------------------------------------------------------------------ #
+# ------------------------------------------------------------------------ #
+# ------------------------------------------------------------------------ #
+#' Data from Ushio (2022) Proc. Royal Soc. B: ASV table (copy numbers)
 #'
 #' @format A data frame with 610 rows and 1197 variables:
 #' @source \url{https://doi.org/10.1098/rspb.2021.2690}
-"ushio_2022_prsb_df_asvmat"
+"ushio_2022_prsb_df_asvtab_copy"
 
 
 #' Data from Ushio (2022) Proc. Royal Soc. B: Climate data
@@ -26,51 +29,118 @@
 "ushio_2022_prsb_df_taxa"
 
 
-#' Data from Ushio (2023) eLife: Rice monitoring data in 2017
+# ------------------------------------------------------------------------ #
+# ------------------------------------------------------------------------ #
+# ------------------------------------------------------------------------ #
+#' Data from Ushio et al. (2023a) eLife: ASV table (copy numbers)
 #'
-#' @format A data frame with 610 rows and 18 variables:
+#' @format A data frame with 550 rows and 856 variables:
+#' @source \url{https://doi.org/10.7554/eLife.85795}
+"ushio_2023a_elife_df_asvtab_copy"
+
+
+#' Data from Ushio et al. (2023a) eLife: ASV table (sequence reads)
+#'
+#' @format A data frame with 550 rows and 856 variables:
+#' @source \url{https://doi.org/10.7554/eLife.85795}
+"ushio_2023a_elife_df_asvtab_read"
+
+
+#' Data from Ushio et al. (2023a) eLife: Sample data
+#'
+#' @format A data frame with 550 rows and 28 variables:
+#' @source \url{https://doi.org/10.7554/eLife.85795}
+"ushio_2023a_elife_df_sample"
+
+
+#' Data from Ushio et al. (2023a) eLife: Taxa table
+#'
+#' @format A data frame with 856 rows and 14 variables:
+#' @source \url{https://doi.org/10.7554/eLife.85795}
+"ushio_2023a_elife_df_taxa"
+
+
+
+# ------------------------------------------------------------------------ #
+# ------------------------------------------------------------------------ #
+# ------------------------------------------------------------------------ #
+#' Data from Ushio et al. (2023a) eLife: ASV table (eDNA copy numbers)
+#'
+#' @format A data frame with 550 rows and 857 variables:
+#' @source \url{https://doi.org/10.7554/eLife.85795}
+"ushio_2023a_elife_df_asvtab_copy"
+
+
+#' Data from Ushio et al. (2023a) eLife: ASV table (eDNA reads)
+#'
+#' @format A data frame with 550 rows and 857 variables:
+#' @source \url{https://doi.org/10.7554/eLife.85795}
+"ushio_2023a_elife_df_asvtab_read"
+
+
+#' Data from Ushio et al. (2023a) eLife: Sample data
+#'
+#' @format A data frame with 550 rows and 857 variables:
+#' @source \url{https://doi.org/10.7554/eLife.85795}
+"ushio_2023a_elife_df_sample"
+
+
+#' Data from Ushio et al. (2023a) eLife: Taxa table
+#'
+#' @format A data frame with 856 rows and 15 variables:
+#' @source \url{https://doi.org/10.7554/eLife.85795}
+"ushio_2023a_elife_df_taxa"
+
+
+
+# ------------------------------------------------------------------------ #
+# ------------------------------------------------------------------------ #
+# ------------------------------------------------------------------------ #
+#' Data from Ushio et al. (2023b) eLife: Rice monitoring data in 2017
+#'
+#' @format A data frame with 610 rows and 20 variables:
 #' @source \url{https://doi.org/10.7554/eLife.87202.1}
 "ushio_2023b_elife_df_rice2017"
 
 
-#' Data from Ushio et al. (2023) eLife: ASV table
+#' Data from Ushio et al. (2023b) eLife: ASV table for 2019
 #'
 #' @format A data frame with 324 rows and 1414 variables:
 #' @source \url{https://doi.org/10.7554/eLife.87202.1}
-"ushio_2023b_elife_df_asvmat"
+"ushio_2023b_elife_df_asvtab_copy"
 
 
-#' Data from Ushio et al. (2023) eLife: Sample data
+#' Data from Ushio et al. (2023b) eLife: Sample data for 2019
 #'
 #' @format A data frame with 324 rows and 44 variables:
 #' @source \url{https://doi.org/10.7554/eLife.87202.1}
 "ushio_2023b_elife_df_sample"
 
 
-#' Data from Ushio et al. (2023) eLife: Taxa table
+#' Data from Ushio et al. (2023b) eLife: Taxa table for 2019
 #'
 #' @format A data frame with 1414 rows and 24 variables:
 #' @source \url{https://doi.org/10.7554/eLife.87202.1}
 "ushio_2023b_elife_df_taxa"
 
 
-#' Data from Ushio (2023) eLife: Rice growth monitoring data in 2019
+#' Data from Ushio (2023b) eLife: Rice growth monitoring data in 2019
 #'
 #' @format A data frame with 405 rows and 16 variables:
 #' @source \url{https://doi.org/10.7554/eLife.87202.1}
-"ushio_2023b_elife_df_rice2019_gr"
+"ushio_2023b_elife_df_rice2019_growth"
 
 
 #' Data from Ushio (2023) eLife: Rice heading monitoring data in 2019
 #'
 #' @format A data frame with 171 rows and 10 variables:
 #' @source \url{https://doi.org/10.7554/eLife.87202.1}
-"ushio_2023b_elife_df_rice2019_hd"
+"ushio_2023b_elife_df_rice2019_head"
 
 
-#' Data from Ushio (2023) eLife: Rice yield data in 2019
+#' Data from Ushio (2023b) eLife: Rice yield data in 2019
 #'
-#' @format A data frame with 81 rows and 23 variables:
+#' @format A data frame with 81 rows and 22 variables:
 #' @source \url{https://doi.org/10.7554/eLife.87202.1}
-"ushio_2023b_elife_df_rice2019_yd"
+"ushio_2023b_elife_df_rice2019_yield"
 
--- R/data_loader.R
+++ R/data_loader.R
@@ -1,27 +1,36 @@
 #' @title Data loader
 #' @description \code{data_loader} Batch loading data set.
-#' @param dataset_name Character. 'ushio_2022_prsb' or 'ushio_2023_elife'.
+#' @param dataset_name Character. 'ushio_2022_prsb', 'ushio_2023a_elife' or 'ushio_2023b_elife'.
 #' @export
 #' @examples
 #' # data_loader()
 data_loader <- function(dataset_name) {
   # Batch loading Ushio (2022) Proceedings B
   if (dataset_name == "ushio_2022_prsb") {
-    data("ushio_2022_prsb_df_asvmat")
+    data("ushio_2022_prsb_df_asvtab_copy")
     data("ushio_2022_prsb_df_climate")
     data("ushio_2022_prsb_df_sample")
     data("ushio_2022_prsb_df_taxa")
   }
 
-  # Batch loading Ushio et al. (2023) eLife
+  # Batch loading Ushio et al. (2023a) eLife
+  if (dataset_name == "ushio_2023a_elife") {
+    data("ushio_2023a_elife_df_asvtab_copy")
+    data("ushio_2023a_elife_df_asvtab_read")
+    data("ushio_2023a_elife_df_sample")
+    data("ushio_2023a_elife_df_taxa")
+  }
+
+  # Batch loading Ushio et al. (2023b) eLife
   if (dataset_name == "ushio_2023b_elife") {
-    data("ushio_2023b_elife_df_asvmat")
+    data("ushio_2023b_elife_df_asvtab_copy")
     data("ushio_2023b_elife_df_sample")
     data("ushio_2023b_elife_df_taxa")
     data("ushio_2023b_elife_df_rice2017")
-    data("ushio_2023b_elife_df_rice2019_gr")
-    data("ushio_2023b_elife_df_rice2019_hd")
-    data("ushio_2023b_elife_df_rice2019_yd")
+    data("ushio_2023b_elife_df_climate2019")
+    data("ushio_2023b_elife_df_rice2019_growth")
+    data("ushio_2023b_elife_df_rice2019_head")
+    data("ushio_2023b_elife_df_rice2019_yield")
   }
 }
 
